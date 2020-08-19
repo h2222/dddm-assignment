7b836b6947e01f84b258f27cd9f31c524a69eff6
h2222 <1311367609@qq.com>
2020-08-19 22:00:00 +0800

    update a1, splite and save data into map

--- a1/Assignment_1_Sample_Test/sample_case_1/AAGenerator.cpp
+++ a1/Assignment_1_Sample_Test/sample_case_1/AAGenerator.cpp
@@ -2,31 +2,31 @@
 #include <fstream>
 #include <string>
 #include <map>
+#include <vector>
+#include <regex>
 
 using namespace std;
 
-vector<string> split(string& s, string& c)
+template <typename T>
+void split(vector<T>& st_result, const string& s, const string& text)
 {
-	string::size_type pos1, pos2;
-	pos1 = s.find(c);
-	pos2 = 0;
-	while(string::npos != pos2)
+	regex st(s);
+	vector<string> result(sregex_token_iterator(begin(text), end(text), st, -1),
+			sregex_token_iterator());
+	for(auto i : result)
 	{
-		v.push_back(s.substr(pos1, pos2-pos1));
-	}
-	pos1 = pos2 + c.size();
-	pos2 = s.find(c, pos1);
-	if (pos1 != s.length())
-	{
-		v.push_
+		st_result.push_back(i);
 	}
 }
 
 
-
-
 int main(int argc, char* argv[])
 {
+	/*
+		that is how we split the string and save key value pairs into the map, which can be done in a second by python ! 
+	*/
+	vector<string> st_result;
+
 	for (int i = 1; i < argc; i++)
 	{
 		string fname = argv[i];
@@ -36,39 +36,76 @@ int main(int argc, char* argv[])
 
 		if (fname.find(att) != string::npos)
 		{
+			string as = "\\s+";
 			cout << fname << endl;	
 			map<string, string> attm;
 			fstream myfile(argv[i]);
 			string line;
+			getline(myfile, line); // jump first line 
 			while(getline(myfile, line))
 			{
-				char lc[line.length()];
-				
-				cout << line << endl;
-				//attm.insert(pair<String, String>())
+				split(st_result, as, line);
+				attm.insert(pair<string, string>(st_result[0], st_result[1]));
+				st_result.clear();
+			}
+			// print map contents
+			map<string, string>::iterator iter;
+			for (iter = begin(attm); iter != end(attm); iter++)
+			{
+				cout << iter->first << ":" << iter->second << endl;
 			}
 		} 
 		else if (fname.find(que) != string::npos)
 		{
+			string qs = ":\\s+";
 			cout << fname << endl;	
 			map<string, string> quem;
 			fstream myfile(argv[i]);
 			string line;
 			while(getline(myfile, line))
 			{
-				cout << line << endl;
+				split(st_result, qs, line);
+				quem.insert(pair<string, string>(st_result[0], st_result[1]));
+				st_result.clear();
 			}
-
+			map<string, string>::iterator iter;
+			for (iter = begin(quem); iter != end(quem); iter++)
+			{
+				cout << iter->first << ":" << iter->second << endl;
+			}
 		}
 		else if (fname.find(acc) != string::npos)
 		{
+			string cs = "\\s+";
+			vector<int> st_result_2;
 			cout << fname << endl;	
-			map<string, string> accm;
+			map<string, vector<int>> accm;
 			fstream myfile(argv[i]);
 			string line;
+			getline(myfile, line);
 			while(getline(myfile, line))
 			{
-				cout << line << endl;
+				split(st_result, cs, line);
+				string key = st_result[0];
+				for (int i = 1; i < st_result.size(); i++)
+				{
+					int a  = stoi(st_result[i]);
+					st_result_2.push_back(a);
+				}
+				accm.insert(pair<string, vector<int>>(key, st_result_2));
+				st_result_2.clear();
+				st_result.clear();
+			}
+			map<string, vector<int>>::iterator iter;
+			for (iter = begin(accm); iter != end(accm); iter++)
+			{
+				cout << iter->first << ": ";
+				vector<int> temp = iter->second;
+				for (int i : temp)
+				{
+					cout << i << " ";
+				}
+				cout << endl;
 			}
 		}
 	}
