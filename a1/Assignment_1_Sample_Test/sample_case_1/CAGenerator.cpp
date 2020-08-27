775194ff2eff38a46f4a21554640796e3f569810
h2222 <1311367609@qq.com>
2020-08-28 01:08:07 +0800

    add CAG

--- a1/Assignment_1_Sample_Test/sample_case_1/CAGenerator.cpp
+++ a1/Assignment_1_Sample_Test/sample_case_1/CAGenerator.cpp
@@ -22,108 +22,102 @@ void split(vector<T>& st_result, const string& s, const string& text)
 }
 
 
-void print_square_matrix(int** matrix_p, int size)
-{
-    for (int i = 0; i < size; i++)
-    {
-        for (int j = 0; j < size; j++)
-        {
-            cout << matrix_p[i][j] << " ";
-        }
-        cout << endl;
-    }
-}
-
-
-
 int bond(int** AA_p, int a, int b, int size)
 {
     int res = 0;
     for (int i = 0; i < size; i++)
     {
-        // cout << "[i, a] -> " << i << " " << a << ", " << "[i, b] -> " << i << " " << b << endl;
-        // cout << AA_p[i][a] << " * " << AA_p[i][b] << endl;
         res += AA_p[i][a] * AA_p[i][b];
     }
-    // cout << endl;
-    // cout << endl;
-    // cout << res << endl;
     return res;
 }
 
 
 int arg_max_index(int* loc, int index)
 {
-    cout << " loc list " << endl;
     int best_loc = 0;
     int best_loc_cont = 1<<31;
     for (int i = 1; i <= index; i++)
     {
-        if (loc[i] > best_loc_cont)
+        if (loc[i] >= best_loc_cont)
         {
             best_loc_cont = loc[i];
             best_loc = i;
         }
     }
-    cout << "best_position: " << best_loc << " loc contribution: " << best_loc_cont << endl;
-    cout << endl;
-    cout << endl;
+    // cout << "best_position: " << best_loc << " loc contribution: " << best_loc_cont << endl;
+    return best_loc;
+}
+
+
+void transfer(int** CA_p, int** AA_p, int index, int best_position, int size)
+{
+    for (int j = index; j > best_position; j--)
+        {
+            CA_p[j] = CA_p[j - 1];
+            int* temp = CA_p[j - 1];
+        }
+        CA_p[best_position] = AA_p[index];
+        int* temp2 = AA_p[index];
 }
 
 
 void generator(int** AA_p, int size)
 {
-    for (int i = 0; i < size; i++)
-    {
-        for (int j = 0; j < size; j++)
-        {
-            cout << AA_p[i][j] << " ";
-        }
-        cout << endl;
-    }
     int** CA_p = new int* [size];
-
+    map<int, int> idx_loc;
+    CA_p[0] = AA_p[0];
+    CA_p[size - 1] = AA_p[size - 1];
     CA_p[1] = AA_p[1];
-    CA_p[2] = AA_p[2];
+    CA_p[2] = AA_p[2];    
     int index = 3;
     while (index < size - 1)
     {
         int* loc = new int[index+1];
         for (int i = 1; i <= index - 1; i++)
         {
-            cout << "(i-1): " << i - 1 << " (i): " << i << " (index): " << index << endl;
-            int a = bond(AA_p, i - 1, index, size);
-            int b = bond(AA_p, index, i, size);
-            int c = bond(AA_p, i - 1, i, size);
-            cout << " a: " << a << " b: " << b << " c: " << c << endl;
-            int cont = 2 * a + 2 * b - 2 * c;
+            // cout << "(i-1): " << i - 1 << " (i): " << i << " (index): " << index << endl;
+            int cont = 2 * bond(AA_p, i - 1, index, size) + 2 * bond(AA_p, index, i, size) - 2 * bond(AA_p, i - 1, i, size);
             loc[i] = cont;
-            cout << cont << endl;
         }
-        cout << "(index-1): " << index - 1 << " (index): " << index << " (index+1): " << index + 1 << endl;
+        // cout << "(index-1): " << index - 1 << " (index): " << index << " (index+1): " << index + 1 << endl;
         int cont = 2 * bond(AA_p, index - 1, index, size) + 2 * bond(AA_p, index, index + 1, size) - 2 * bond(AA_p, index - 1, index + 1, size);
-        cout << cont << endl; 
         loc[index] = cont;
         int best_position = arg_max_index(loc, index);  // GET THE BEST POSITION OF THE CA COLUMN.
+        idx_loc.insert(pair<int, int>(index, best_position));
+        transfer(CA_p, AA_p, index, best_position, size);
         index++;
     }
-
+    // transpose
     for (int i = 0; i < size; i++)
     {
-        cout << *CA_p[2] << " ";
-        CA_p[2]++;
-    }   
-    cout << endl; 
-    // for (int i = 0; i < size; i++)
-    // {
-    //     for (int j = 0; j < size; j++)
-    //     {
-    //         cout << "i: " << i << " j:" << j << endl;
-    //         CA_a = AA_p[0];
-    //         CA_b = AA_p[2];  
-
-    //     }
-    // }
+        for(int j = 0; j < i; j++)
+        {
+            int temp3 = CA_p[i][j];
+            CA_p[i][j] = CA_p[j][i];
+            CA_p[j][i] = temp3;
+        }
+    }
+	map<int, int>::iterator iter;
+    int** res = new int* [size];
+    res[0] = CA_p[0];
+    res[size - 1] = CA_p[size - 1];
+    res[1] = CA_p[1];
+    res[2] = CA_p[2];  
+    for(iter = begin(idx_loc); iter != end(idx_loc); iter++)
+    {
+        int index = iter->first;
+        int best_position = iter->second;
+        transfer(res, CA_p, index, best_position, size);
+    }
+    for (int i = 1; i < size - 1; i++)
+    {
+        for (int j = 1; j < size - 1; j++)
+        {
+            cout << res[i][j] << " ";
+        }
+        cout << endl;
+    }
 }
 
 
@@ -132,7 +126,6 @@ int main(int argc, char* argv[])
     fstream myfile(argv[1]);
     string line;
     vector<string> line_save;
-    
     int size = 0;
     while (getline(myfile, line))
     {
@@ -159,21 +152,6 @@ int main(int argc, char* argv[])
             }
         }
     }
-
-    print_square_matrix(AA_p, size);
-
-
-
-    // int res = bond(AA_p, 1, 2, size);
-    // cout << "res: " << res << endl;
     generator(AA_p, size);
-
-    // int (*line_p)[y_size] = AA_p;
-
-    // for (int i = 0; i < y_size; i++)
-    // {
-    //     cout << *line_p[0] << " "; // print line
-    //     line_p++;   
-    // }
     return 0;
 }
